a1ec5fff5ebc98c624137222610fe9f25f7115cc
aidanblack <aidanblack@me.com>
2021-03-31 12:24:27 -0500

    Api Key

--- companion/index.js
+++ companion/index.js
@@ -1,3 +1,5 @@
 import * as weather from 'fitbit-weather/companion';
 
+/* Api Key can be obtained from openweathermap.com */
 weather.setup({ provider : weather.Providers.openweathermap, apiKey : '<API KEY>' });
+
