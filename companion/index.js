a5f689e5c0159025b4a02f1f9b80fe62b2687f33
aidanblack <aidanblack@me.com>
2021-06-01 16:28:45 -0500

    Fix API Key import

--- companion/index.js
+++ companion/index.js
@@ -2,7 +2,7 @@ import { settingsStorage } from "settings";
 import * as messaging from "messaging";
 import { me as companion } from "companion";
 import * as weather from 'fitbit-weather/companion';
-import * as weatherKey from 'weather.js';
+import * as weatherKey from './weather';
 
 /* Api Key can be obtained from openweathermap.com */
 weather.setup({ provider: weather.Providers.openweathermap, apiKey: weatherKey.apiKey });
